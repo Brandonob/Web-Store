6eee4917ed698b52b2b94140f83a43ce47763867
BrandonBrown <brandonbrown@pursuit.org>
2023-02-27 18:59:09 -0500

    add pages controller and erb template and get react to show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   namespace :api do 
-    
+    resources :orders, param: :slug
+    resources :products, only: [:create, :destroy, :update]
   end
+
+  get '*path', to: 'pages#index', via: :all
 end
