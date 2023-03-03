dc96154e57c068e6ba18d51470649f327536faa4
BrandonBrown <brandonbrown@pursuit.org>
2023-03-02 21:22:24 -0500

    create admin login route and install chakra ui

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
+import Login from './Admin/Login/Login'
 
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <Router>
         <Routes>
           <Route exact path="/" element={<Home />}/>
+          <Route exact path="/admin/login" element={<Login />}/>
         </Routes>
     </Router>
   )
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,18 +4,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import PropTypes from 'prop-types'
 import App from '../components/App'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
-    // <Router>
-    //   <Routes>
-    //     <Route exact path='/' element={<App />} />
-    //   </Routes>
-    // </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
